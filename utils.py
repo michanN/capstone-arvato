3680f23f16dc7e3e18a83d4359305dc41fb85b16
Michel <michel.naslund@gmail.com>
2019-01-11 13:58:06 +0100

    Start with pre-processing

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import datetime
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
+import matplotlib.pyplot as plt
+import seaborn as sns
